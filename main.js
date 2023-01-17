e8b9697b420ae26dee839ec450c55aa891a8fda0
Sam11-ss <84765714+Sam11-ss@users.noreply.github.com>
2023-01-17 21:01:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
-    video.size(550,500);
-    canvas=createCanvas(550,450);
-    canvas.position(700,80);
+    video.size(550,400);
+    canvas=createCanvas(550,400);
+    canvas.position(700,130);
     posenet = ml5.poseNet(video , modelLoaded);
     posenet.on('pose',gotPoses);
 }
@@ -12,5 +15,18 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length > 0){
     console.log(results);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;   
+    difference=floor(leftWristX-rightWristX);
+
+console.log("Left Wrist x is "+leftWristX+" Right wrist x is "+rightWristX+" Difference is "+difference);
 }
+}
+function draw(){
+    document.getElementById("font_size").innerHTML="The size of the font is - " + difference;
+    background("purple");
+    textSize(difference);
+    fill("black");
+    text("Hello!" , 50, 400);
 }
